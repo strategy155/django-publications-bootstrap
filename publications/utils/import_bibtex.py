4740d494e835daf827dca83ceaad319cde13d206
Lucas Theis <lucas@theis.io>
2014-08-08 23:49:25 -0700

    Decode special characters such as umlauts when importing bibtex.

--- publications/utils/import_bibtex.py
+++ publications/utils/import_bibtex.py
@@ -26,25 +26,47 @@ MONTHS = {
 	'nov': 11, 'november': 11,
 	'dec': 12, 'december': 12}
 
+special_chars = (
+	(r'\"{a}', 'ä'), (r'{\"a}', 'ä'), (r'\"a', 'ä'), (r'H{a}', 'ä'),
+	(r'\"{A}', 'Ä'), (r'{\"A}', 'Ä'), (r'\"A', 'Ä'), (r'H{A}', 'Ä'),
+	(r'\"{o}', 'ö'), (r'{\"o}', 'ö'), (r'\"o', 'ö'), (r'H{o}', 'ö'),
+	(r'\"{O}', 'Ö'), (r'{\"O}', 'Ö'), (r'\"O', 'Ö'), (r'H{O}', 'Ö'),
+	(r'\"{u}', 'ü'), (r'{\"u}', 'ü'), (r'\"u', 'ü'), (r'H{u}', 'ü'),
+	(r'\"{U}', 'Ü'), (r'{\"U}', 'Ü'), (r'\"U', 'Ü'), (r'H{U}', 'Ü'),
+	(r'{‘a}', 'à'), (r'\‘A', 'À'),
+	(r'{‘e}', 'è'), (r'\‘E', 'È'),
+	(r'{‘o}', 'ò'), (r'\‘O', 'Ò'),
+	(r'{‘u}', 'ù'), (r'\‘U', 'Ù'),
+	(r'{’a}', 'á'), (r'\’A', 'Á'),
+	(r'{’e}', 'é'), (r'\’E', 'É'),
+	(r'{’o}', 'ó'), (r'\’O', 'Ó'),
+	(r'{’u}', 'ú'), (r'\’U', 'Ú'),
+	(r'\`a', 'à'), (r'\`A', 'À'),
+	(r'\`e', 'è'), (r'\`E', 'È'),
+	(r'\`u', 'ù'), (r'\`U', 'Ù'),
+	(r'\`o', 'ò'), (r'\`O', 'Ò'),
+	(r'\^o', 'ô'), (r'\^O', 'Ô'),
+	(r'\ss', 'ß'),
+	(r'\ae', 'æ'), (r'\AE', 'Æ'))
+
 def _fix_text_grouping(record):
 	'''Turns \text{\'e} into the correct \text{{\'e}} (\text{} being the amsmath command).
 	'''
-	
+
 	def wrap_in_group_re_clble(match):
 		return '{%s}' % match
-	
+
 	def wrap_in_group(text):
 		return re.sub(r'''(\\text{)(\\.*?)(})''', lambda match: match.expand(r'\1{\2}\3'), text)
-	
+
 	for key, value in record.iteritems():
-		
 		if isinstance(value, list):
 			new_value = [wrap_in_group(element) for element in value]
 		else:
 			new_value = wrap_in_group(value)
-		
+
 		record[key] = new_value
-	
+
 	return record
 
 def _bibtexparser_customizations(record):
@@ -57,15 +79,18 @@ def import_bibtex(bibtex, bibtexparser_customization=None):
 	'''
 	Import BibTeX data from a file-like object or a string
 	'''
-	
+
 	# BibTexParser expects a utf-8 byte-string
 	if isinstance(bibtex, unicode):
 		bibtex = bibtex.encode('utf-8')
-		
+
+	for key, value in special_chars:
+		bibtex = bibtex.replace(key, value)
+
 	# add trailing newline if not present, otherwise bibtexparser will not parse fully
 	if not bibtex.endswith('\n'):
 		bibtex = bibtex + '\n'
-		
+
 	# try to parse BibTex
 	def _cust(record):
 		record = _bibtexparser_customizations(record)
@@ -86,7 +111,7 @@ def import_bibtex(bibtex, bibtexparser_customization=None):
 		'year']
 
 	saved_publications = []
-	
+
 	# try adding publications
 	for entry in bib:
 		# first fix integers - 'year' needs to be checked for int-ness in particular
@@ -101,14 +126,14 @@ def import_bibtex(bibtex, bibtexparser_customization=None):
 		   entry.has_key('author') and \
 		   entry.has_key('year') and \
 		   entry['year'] != None:
-			
+
 			# join parsed authors
 			def reverse_and_unseparate_name(n):
 				if ',' in n:
 					return ' '.join([p.strip() for p in n.split(',')][::-1])
-				return n			
+				return n
 			authors = ', '.join([reverse_and_unseparate_name(n) for n in entry['author']])
-			
+
 			# add missing keys
 			keys = [
 				'journal',
@@ -141,9 +166,10 @@ def import_bibtex(bibtex, bibtexparser_customization=None):
 			if type_id is None:
 				errors.append('Type "' + entry['type'] + '" unknown.')
 				continue
-			
-			# Handle case where 'Volume' key contains DOI reference
+
+			# handle case where 'volume' key contains DOI reference
 			volume = entry.get('volume', None)
+
 			if volume:
 				try:
 					int(volume)
@@ -152,7 +178,7 @@ def import_bibtex(bibtex, bibtexparser_customization=None):
 						entry['doi'] = entry['volume']
 					entry['volume'] = None
 
-			# Handle case where 'Number' key is not a number (eg. {-})
+			# handle case where 'number' key is not a number (eg. {-})
 			number = entry.get('number', None)
 			if number:
 				try:
@@ -177,33 +203,40 @@ def import_bibtex(bibtex, bibtexparser_customization=None):
 				isbn=unicode(entry['isbn']),
 				abstract=unicode(entry['abstract']),
 				keywords=unicode(u', '.join(entry['keywords'])))
-			
+
 			publication = Publication(**publication_data)
+
 			try:
 				converted_data = model_to_dict(publication, exclude=['lists', 'image', 'pdf', 'banner', 'id', 'citekey'])
 				publication = Publication.objects.get(**converted_data)
 				saved_publications.append(publication)
+
 			except Publication.DoesNotExist:
 				publication.citekey = entry['id']
+
 				try:
 					publication.save()
 					saved_publications.append(publication)
+
 				except Exception, e:
 					# show error message
 					key = publication.citekey
 					if not key:
 						key = '<unnamed>'
 					errors.append('An error occurred saving publication "%s": %s' % (key, e))
-			
+
 		else:
 			key = entry['id'] if 'id' in entry else '<unnamed>'
 			from pprint import pprint
 			pprint(entry)
 			missing_keys = []
+
 			for k in ('title', 'author', 'year'):
 				if not k in entry or entry[k] == None:
 					missing_keys.append(k)
+
 			errors.append('BibTeX entry "%s" is missing following mandatory keys: %s' % (key, ', '.join(missing_keys)))
+
 			continue
 
 	return saved_publications, errors
