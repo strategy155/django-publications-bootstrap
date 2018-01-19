fb87613da8d7c627544c5d299e04c3e9fa2816a7
MaxBo <max.bohnet@tuhh.de>
2018-01-19 10:50:37 +0100

    fix bug in Month field

--- publications_bootstrap/admin_views/import_bibtex.py
+++ publications_bootstrap/admin_views/import_bibtex.py
@@ -13,54 +13,48 @@ from django.db import transaction
 from ..utils import import_bibtex as do_import_bibtex
 from ..models import Publication, Type
 
-# mapping of months
-MONTHS = {
-    'jan': 1, 'january': 1,
-    'feb': 2, 'february': 2,
-    'mar': 3, 'march': 3,
-    'apr': 4, 'april': 4,
-    'may': 5,
-    'jun': 6, 'june': 6,
-    'jul': 7, 'july': 7,
-    'aug': 8, 'august': 8,
-    'sep': 9, 'september': 9,
-    'oct': 10, 'october': 10,
-    'nov': 11, 'november': 11,
-    'dec': 12, 'december': 12}
-
-
 
 def import_bibtex(request):
     if request.method == 'POST':
         # try to import BibTex
-        bibtex = request.POST['bibliography']
-
-        with transaction.atomic():
-            publications, errors = do_import_bibtex(bibtex)
-
-        status = messages.SUCCESS
-        if len(publications) == 0:
-            status = messages.ERROR
-            msg = 'No publications were added, %i errors occurred' % len(errors)
-        elif len(publications) > 1:
-            msg = 'Successfully added %i publications (%i skipped due to errors)' % (len(publications), len(errors))
-        else:
-            msg = 'Successfully added %i publication (%i error(s) occurred)' % (len(publications), len(errors))
-
-        # show message
-        messages.add_message(request, status, msg)
-
-        for error in errors:
-            messages.add_message(request, messages.ERROR, error)
+        publications = parse_upload_bibtex(request)
 
         # redirect to publication listing
         return HttpResponseRedirect('../')
     else:
-        return render(request,
-                      'admin/publications_bootstrap/import_bibtex.html',
-                      {'title': 'Import BibTex',
-                       'types': Type.objects.all(),
-                       'request': request},
-                      )
+        response = get_response(request)
+        return response
+
+def get_response(request):
+    response = render(request,
+                  'admin/publications_bootstrap/import_bibtex.html',
+                  {'title': 'Import BibTex',
+                   'types': Type.objects.all(),
+                   'request': request},
+                  )
+    return response
+
+def parse_upload_bibtex(request):
+    bibtex = request.POST['bibliography']
+
+    with transaction.atomic():
+        publications, errors = do_import_bibtex(bibtex)
+
+    status = messages.SUCCESS
+    if len(publications) == 0:
+        status = messages.ERROR
+        msg = 'No publications were added, %i errors occurred' % len(errors)
+    elif len(publications) > 1:
+        msg = 'Successfully added %i publications (%i skipped due to errors)' % (len(publications), len(errors))
+    else:
+        msg = 'Successfully added %i publication (%i error(s) occurred)' % (len(publications), len(errors))
+
+    # show message
+    messages.add_message(request, status, msg)
+
+    for error in errors:
+        messages.add_message(request, messages.ERROR, error)
+
+    return publications
 
 import_bibtex = staff_member_required(import_bibtex)
--- publications_bootstrap/models/publication.py
+++ publications_bootstrap/models/publication.py
@@ -7,7 +7,7 @@ from django.db import models
 from django.utils.http import urlquote_plus
 from django.utils.translation import ugettext_lazy as _
 from django_countries.fields import CountryField
-from echoices.enums import EChoice, EOrderedChoice
+from echoices.enums import EChoice, EOrderedChoice, EChoiceMeta
 from echoices.fields import make_echoicefield
 
 from ..fields import NullCharField, PagesField
@@ -15,6 +15,15 @@ from ..models import Type
 
 if 'django.contrib.sites' in settings.INSTALLED_APPS:
     from django.contrib.sites.models import Site
+
+
+class EChoiceMetaInt(EChoiceMeta):
+    def __getitem__(cls, value):
+        try:
+            value = int(value)
+        except ValueError:
+            pass
+        return super().__getitem__(value)
 
 
 class Publication(models.Model):
@@ -27,7 +36,8 @@ class Publication(models.Model):
         app_label = 'publications_bootstrap'  # Fix for Django<1.7
 
     # names shown in admin area
-    class EMonths(EOrderedChoice):
+    class EMonths(EOrderedChoice, metaclass=EChoiceMetaInt):
+
         JAN = (1, _('January'), 'Jan')
         FEB = (2, _('February'), 'Feb')
         MAR = (3, _('March'), 'Mar')
